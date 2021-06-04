4a96bd7a336893838cf64ca64142220cc68ae68a
Chintan Trivedi <chintantrivedi247@gmail.com>
2021-06-03 20:20:53 -0400

    uploading the code with screenshot of unit testing

---
+++ IsItALeapYear/IsItALeapYear/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IsItALeapYear
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool IsItALeapYear(int input)
+        {
+            return (input % 400 == 0) || ((input%100 != 0) && (input % 4 == 0)) ? true : false;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Lab2-LeapYear
 This project is based on unit testing for the subject code CP - 380 ASP .NET. 
+
+I have created repository and pasted whole folder inside this repo with the successful screenshot.
